e87c7be52ef54c7d044793386b186f2a0aba4b5f
Jonathan Nix <jrn11a@acu.edu>
2016-04-15 09:14:11 -0500

    added main file

---
+++ main.py
@@ -0,0 +1,17 @@
+#import files
+
+def main():
+	while(True):
+		ans = input('Choose game')
+		if ans==1:
+			#game 1 function
+		elif ans=2:
+			#game 2 function
+		elif ans=3:
+			#game 3 function
+		elif ans=4:
+			#game 4 function
+		elif ans=5:
+			break
+
+main()
